495aac4dbe55f7c20de2e7017259da3cb9cdb29f
mhertovm <mhertovmasyan904@gmail.com>
2025-07-10 01:17:47 +0400

    added db create

--- db.js
+++ db.js
@@ -1,11 +1,37 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 
+(async () => {
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+        });
+
+        await connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\`;`);
+        await connection.end()
+
+    } catch (err) {
+        console.error("Database connection failed:", err.message);
+    }
+})()
+
 const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 });
+
+(async () => {
+    try {
+        const connection = await db.getConnection();
+        console.log("Database connection successful");
+        connection.release()
+    } catch (err) {
+        console.error("Database connection failed:", err.message);
+    }
+})();
 
 module.exports = db;
